d5b330284cb1b0a580df4894bf7dc0f20c079524
tkoulal <tkoulal>
2023-11-22 01:52:07 +0100

    fixed if nb <= base_len

--- tools/ft_putunsigned.c
+++ tools/ft_putunsigned.c
@@ -7,7 +7,7 @@ int ft_putunsigned(unsigned long nb)
 
     base = "0123456789";
     count = 0;
-    if (nb >= 16)
+    if (nb >= 10)
         count += ft_putunsigned(nb / 10);
     count += ft_putchar(base[nb % 10]);
     return (count);
